1805efff78375a2e909a10c1a04bb4b87f64ab3c
freedomzzzii <theeraporn.tia@gmail.com>
2021-04-07 00:56:07 +0700

    feat: create action filter data

--- src/common/constant.tsx
+++ src/common/constant.tsx
@@ -1,11 +1,13 @@
-import { initialStateType } from './type'
+import { initialStateType, initialStateWithPayload } from './type'
 
 type constantTypes = {
   host: string | undefined,
   GET_LIST_PHOTO_REQUEST: string,
   GET_LIST_PHOTO_SUCCESS: string,
   GET_LIST_PHOTO_FAILURE: string,
+  LIST_PHOTO: string,
   initialState: initialStateType,
+  initialStateWithPayload: initialStateWithPayload,
 };
 
 const constant: constantTypes = {
@@ -16,8 +18,12 @@ const constant: constantTypes = {
   GET_LIST_PHOTO_REQUEST: 'GET_LIST_PHOTO_REQUEST',
   GET_LIST_PHOTO_SUCCESS: 'GET_LIST_PHOTO_SUCCESS',
   GET_LIST_PHOTO_FAILURE: 'GET_LIST_PHOTO_FAILURE',
+  LIST_PHOTO: 'LIST_PHOTO',
   // state redux
   initialState: {},
+  initialStateWithPayload: {
+    payload: [],
+  },
 };
 
 export default constant;
--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -4,8 +4,10 @@ import constant from '../../common/constant';
 
 // photo
 const fetchGetListPhoto = createAction(constant.GET_LIST_PHOTO_REQUEST);
+const fetchFilterListPhoto = createAction(constant.LIST_PHOTO);
 
 export {
   // photo
   fetchGetListPhoto,
+  fetchFilterListPhoto,
 };
